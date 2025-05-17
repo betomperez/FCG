9949d63e2f4cdf7912903904320222731d1c544a
betomperez <betomperez@gmail.com>
2025-05-17 11:10:58 -0300

    Feat: Criada estrutura para o teste do objeto de valor Password. [Red]

---
+++ tests/FCG.Tests.Domain/ValueObjects/PasswordTests.cs
@@ -0,0 +1,31 @@
+﻿namespace FCG.Tests.Domain.ValueObjects
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void Constructor_ValidPassword_CreatesPassword()
+        {
+            // Arrange & Act
+            // Password password = new Password("Senha@123");
+
+            // Assert
+            // Assert.NotNull(password);
+            Assert.Fail("To be implemented");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("S1@")]
+        [InlineData("Password1")]
+        [InlineData("Pa$$word")]
+        [InlineData("12345678")]
+        public void Constructor_InvalidPassword_ThrowsException(string invalidPassword)
+        {
+            // Arrange & Act
+            // Password password = new Password(invalidPassword);
+
+            // Assert
+            Assert.Fail("To be implemented");
+        }
+    }
+}

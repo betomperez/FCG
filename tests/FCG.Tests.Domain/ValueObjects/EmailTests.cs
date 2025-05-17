977669ee62a566a1cdf773d689f236a76dbbd93c
betomperez <betomperez@gmail.com>
2025-05-17 07:51:16 -0300

    Feat: Criada estrutura para o teste do objeto de valor Email. [Red]

---
+++ tests/FCG.Tests.Domain/ValueObjects/EmailTests.cs
@@ -0,0 +1,37 @@
+﻿namespace FCG.Tests.Domain.ValueObjects;
+
+public class EmailTests
+{
+    [Fact]
+    public void Constructor_ValidAddress_CreatesEmail()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Fail("This test has not been implemented yet.");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("invalid-email")]
+    public void Constructor_InvalidAdress_ThrowsArgumentException(string invalidEmail)
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Fail("This test has not been implemented yet.");
+    }
+
+    [Fact]
+    public void ToString_ValidEmail_ReturnsEmailAddress()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Fail("This test has not been implemented yet.");
+    }
+}
